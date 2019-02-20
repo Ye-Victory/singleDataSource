69dcf97c791b54cc6730d845fcf987276d982c45
victory <victory@qq.com>
2019-02-20 20:50:14 +0800

    init singleDataSource

---
+++ src/main/java/com/zousl/singleDataSource/SingleDataSourceApplication.java
@@ -0,0 +1,13 @@
+package com.zousl.singleDataSource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleDataSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleDataSourceApplication.class, args);
+	}
+
+}
